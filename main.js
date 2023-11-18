3737e7732b0aafda37db1bdd86cf99fb1a6822eb
laxman989 <laxmanchoudhary5685@gmail.com>
2023-11-19 01:33:54 +0530

    validation logic added and code refactor

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
+// package and service section reference.
 const packageSection = document.getElementById('package-section');
 const serviceSection = document.getElementById('services-section');
 const packageBookingBtn = document.getElementById('packageBookingBtn');
 
+// input elements reference.
 const whereTo = document.getElementById('whereTo');
 const persons = document.getElementById('persons');
 const startDate = document.getElementById('startDate');
 const endDate = document.getElementById('endDate');
 const description = document.getElementById('description');
 
+// error elements reference for input fields.
+const whereToError = document.getElementById('whereToError');
+const personsError = document.getElementById('personsError');
+const startDateError = document.getElementById('startDateError');
+const endDateError = document.getElementById('endDateError');
+const descriptionError = document.getElementById('descriptionError');
+
+// booking form reference
 const bookingForm = document.getElementById('bookingForm');
 
 const packages = [
@@ -61,39 +71,85 @@ const services = [
     },
 ]
 
-function handleFormSubmit() {
-    console.log(Date.now().toLocaleString());
-    console.log(startDate.value);
-    if(!whereTo.value || !persons.value || !startDate.value || !endDate.value || !description.value) {
-        alert("Error: Please enter value in all the fields.")
-        return
+// functions to validate the form input fields.
+function validateNotEmpty(value, errorElement, errorMessage) {
+    if (!value) {
+        errorElement.textContent = errorMessage;
+        return false;
+    } else {
+        errorElement.textContent = "";
+        return true;
     }
-    if(persons.value < 1) {
-        alert("Error: Please add at least one person.");
-        return
-    }
-    if(startDate.value < Date.now()) {
-        alert("Error: Start Date cannot be in the past.");
-        return
-    }
-    if(endDate.value < Date.now()) {
-        alert("Error: End Date cannot be cannot be in the past.");
-        return
-    }
-    if(startDate.value > endDate.value) {
-        alert("Error: Start Date cannot be after End Date.");
-        return
-    }
-    if(description.value.length < 50) {
-        alert("Error: Description length must be at least 50 characters.");
-        return
-    }
-    else {
-        alert(`Thank You! for using Travel.com. Your package has been booked successfully for ${whereTo.value}.`);
+}
 
+function validatePositiveInteger(value, errorElement, errorMessage) {
+    if (value < 1) {
+        errorElement.textContent = errorMessage;
+        return false;
+    } else {
+        errorElement.textContent = "";
+        return true;
     }
-};
+}
 
+function validateDateNotInPast(value, currentDateValue, errorElement, errorMessage) {
+    if (value < currentDateValue) {
+        errorElement.textContent = errorMessage;
+        return false;
+    } else {
+        errorElement.textContent = "";
+        return true;
+    }
+}
+
+function validateEndDateAfterStartDate(endDateValue, startDateValue, errorElement, errorMessage) {
+    if (endDateValue < startDateValue) {
+        errorElement.textContent = errorMessage;
+        return false;
+    } else {
+        errorElement.textContent = "";
+        return true;
+    }
+}
+
+function validateDescriptionLength(value, errorElement, errorMessage, minLength) {
+    if (value.length < minLength) {
+        errorElement.textContent = errorMessage;
+        return false;
+    } else {
+        errorElement.textContent = "";
+        return true;
+    }
+}
+
+// function to handle booking form.
+function handleFormSubmit(e) {
+    var currentDate = new Date().toISOString().split('T')[0];
+
+    var isValid = true;
+
+    if (!whereTo.value || !persons.value || !startDate.value || !endDate.value || !description.value) {
+        isValid &= validateNotEmpty(whereTo.value, whereToError, "Error: Complete this field.");
+        isValid &= validateNotEmpty(persons.value, personsError, "Error: Complete this field.");
+        isValid &= validateNotEmpty(startDate.value, startDateError, "Error: Complete this field.");
+        isValid &= validateNotEmpty(endDate.value, endDateError, "Error: Complete this field.");
+        isValid &= validateNotEmpty(description.value, descriptionError, "Error: Complete this field.");
+    } else {
+        isValid &= validatePositiveInteger(persons.value, personsError, "Error: Please add at least one person.");
+        isValid &= validateDateNotInPast(startDate.value, currentDate, startDateError, "Error: Start Date cannot be in the past.");
+        isValid &= validateDateNotInPast(endDate.value, currentDate, endDateError, "Error: End Date cannot be in the past.");
+        isValid &= validateEndDateAfterStartDate(endDate.value, startDate.value, endDateError, "Error: End Date cannot be prior to Start Date.");
+        isValid &= validateDescriptionLength(description.value, descriptionError, "Error: Description must be at least 50 characters.", 50);
+    }
+
+    if (isValid) {
+        alert(`Thank You!\nYour package has been booked successfully for ${whereTo.value}.`);
+        bookingForm.reset();
+    }
+    e.preventDefault();
+}
+
+// function to create HTML package card.
 function createPackage() {
     packages.map((package) => {
         let packageCard = document.createElement('div');
@@ -128,11 +184,12 @@ function createPackage() {
     })
 };
 
+// function to create HTML service card.
 function createService() {
     services.map((service) => {
         let serviceCard = document.createElement('div');
         serviceCard.classList.add('service-item');
-    
+
         let serviceName = document.createElement('h3');
         serviceName.textContent = service.name;
         let serviceDescription = document.createElement('p');
@@ -146,12 +203,36 @@ function createService() {
 createPackage();
 createService();
 
+// event listener to submit the form on button click.
 packageBookingBtn.addEventListener('click', (e) => {
     e.preventDefault();
     handleFormSubmit();
 });
+
+// event listener to submit the form on form submit.
 bookingForm.addEventListener('submit', (e) => {
     e.preventDefault();
     handleFormSubmit();
     bookingForm.reset();
+});
+
+// Event listeners to remove the error message from input fields when value is updated.
+document.getElementById('whereTo').addEventListener('input', function () {
+    whereToError.textContent = "";
+});
+
+document.getElementById('persons').addEventListener('input', function () {
+    personsError.textContent = "";
+});
+
+document.getElementById('startDate').addEventListener('input', function () {
+    startDateError.textContent = "";
+});
+
+document.getElementById('endDate').addEventListener('input', function () {
+    endDateError.textContent = "";
+});
+
+document.getElementById('description').addEventListener('input', function () {
+    descriptionError.textContent = "";
 });
